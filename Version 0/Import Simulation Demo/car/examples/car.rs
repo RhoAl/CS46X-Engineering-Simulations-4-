3232dc844debed684b6a4018567ab1ba1885c9e4
CameronCrutcher <77303516+CameronCrutcher@users.noreply.github.com>
2024-01-31 16:03:17 -0800

    Add files via upload
    UI implementation

--- Version 0/Import Simulation Demo/car/examples/car.rs
+++ Version 0/Import Simulation Demo/car/examples/car.rs
@@ -1,15 +1,18 @@
 use bevy::prelude::*;
-
+//use std::env;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
 use bevy_integrator::{SimTime, Solver};
 use car::{
     build::{build_car, car_startup_system},
     environment::build_environment,
     setup::{camera_setup, simulation_setup},
+    ui::{ui_example_system, stat_ui},
 };
 use rigid_body::plugin::RigidBodyPlugin;
 
 // Main function
 fn main() {
+    //env::set_var("RUST_BACKTRACE", "1");
     let car_definition = build_car();
     // Create App
     App::new()
@@ -20,8 +23,14 @@ fn main() {
             environment_setup: vec![camera_setup],
             name: "car_demo".to_string(),
         })
+        .add_plugins(EguiPlugin)
         .insert_resource(car_definition)
+        .add_systems(Update, ui_example_system)
+        .add_systems(Update, stat_ui)
         .add_systems(Startup, car_startup_system)
         .add_systems(Startup, build_environment)
         .run();
 }
+
+
+
