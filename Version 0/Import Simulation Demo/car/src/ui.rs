48fdb0b9e0601d48e5a48ad8b42bdeb4eec5aba1
Alexander Rhoads <91506537+RhoAl@users.noreply.github.com>
2024-05-01 19:04:09 -0700

    Add files via upload
    Added Cameron's Speed UI

--- Version 0/Import Simulation Demo/car/src/ui.rs
+++ Version 0/Import Simulation Demo/car/src/ui.rs
@@ -1,6 +1,12 @@
 use bevy::prelude::*;
-use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy_egui::{egui, EguiContexts};
 use crate::build::CarDefinition;
+use crate::control::CarControl;
+use rigid_body::{
+    joint::Joint,
+    sva::{Force, Vector},
+};
+use crate::tire::PointTire;
 
 pub fn ui_example_system(mut contexts: EguiContexts) {
     egui::Window::new("Dev Tools").show(contexts.ctx_mut(), |ui| {
@@ -20,16 +26,28 @@ pub fn ui_example_system(mut contexts: EguiContexts) {
     });
 }
 
-pub fn stat_ui(mut contexts: EguiContexts, mut car:ResMut<CarDefinition>) {
+pub fn stat_ui(mut contexts: EguiContexts, mut car:ResMut<CarDefinition>, carcontrol:ResMut<CarControl>, mut tire_query: Query<&mut PointTire>, mut query_joints: Query<&mut Joint>) {
     egui::Window::new("Stats").show(contexts.ctx_mut(), |ui|{
         ui.horizontal(|ui|{
-            ui.add(egui::DragValue::new(&mut car.wheel.coefficient_of_friction)
-                .speed(0.1)
-                .clamp_range(0..=10)
-            );
-            ui.label("Wheel Friction Coefficient: ");
-            ui.label(car.wheel.coefficient_of_friction.to_string());
-            
+            let mut RoundSpeedValue = 0.0;
+            ui.label("Speed: ");
+            for mut tire in tire_query.iter_mut() {
+               if let Ok([mut joint]) =
+                    query_joints.get_many_mut([tire.joint_entity()])
+                {
+                let mut SpeedValue = joint.qd.abs();
+                RoundSpeedValue = (SpeedValue * 10.0).round() / 10.0;
+
+                }
+            }
+            ui.label(RoundSpeedValue.to_string());
+
+            //ui.label(SpeedValue.to_string());
+            ui.label("Throttle: ");
+            let mut Throttle = carcontrol.throttle;
+            let mut RoundThrottleValue = (Throttle * 100.0).round() / 100.0;
+            ui.label(RoundThrottleValue.to_string());
+
         });
     });
 }
